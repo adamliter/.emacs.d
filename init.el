85ec6bd5c46d3bc065d96544036f39fd76a8f0bc
Adam Liter <adam.liter@gmail.com>
2014-06-09 17:10:07 -0400

    Transparent background and color theme

--- init.el
+++ init.el
@@ -7,13 +7,50 @@
 ;; Load the Emacs pacakges that I want
 (load "~/.emacs.d/my-loadpackages.el")
 
+;; Transparent background and dark color theme
+(set-frame-parameter (selected-frame) 'alpha '(90 90))
+(add-to-list 'default-frame-alist '(alpha 90 90))
+(load-theme 'manoj-dark t)
+
+;; Add texbin to path so we can compile with LaTeX
+(setenv "PATH" (concat (getenv "PATH") ":/usr/texbin"))
+(setq exec-path (append exec-path '("/usr/texbin")))
+
+;; Make default compiler for LaTeX editing `pdflatex`
+(setq TeX-PDF-mode t)
+
+;; Open documents compiled with LaTeX in Skim
+;; with SyncTeX enabled
+;; See http://tex.stackexchange.com/questions/11613/launching-an-external-pdf-viewer-from-emacs-auctex-on-a-mac-osx-fails
+(add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
+
+(setq TeX-source-correlate-method 'synctex)
+
+(add-hook 'LaTeX-mode-hook
+      (lambda()
+        (add-to-list 'TeX-expand-list
+             '("%q" skim-make-url))))
+
+(defun skim-make-url () (concat
+        (TeX-current-line)
+        " "
+        (expand-file-name (funcall file (TeX-output-extension) t)
+            (file-name-directory (TeX-master-file)))
+        " "
+        (buffer-file-name)))
+
+(setq TeX-view-program-list
+      '(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline %q")))
+
+(setq TeX-view-program-selection '((output-pdf "Skim")))
+
 ;; Line numbers in every buffer
 (global-linum-mode 1)
 
 ;; IDO mode
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
-(ido-mode 1) 
+(ido-mode 1)
 
 ;; org-mode
 (require 'org)
