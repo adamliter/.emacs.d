50137b11c0a9e3baf7087ef41a491b4c6e89d254
Adam Liter <adam.liter@gmail.com>
2014-11-19 23:37:31 -0500

    Some basic AUCTeX options to start, including using arara

--- my-auctex-options.el
+++ my-auctex-options.el
@@ -5,33 +5,14 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Make default compiler for LaTeX editing `pdflatex`
-(setq TeX-PDF-mode t)
+;; For document parsing
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
 
-;; Open documents compiled with LaTeX in Skim
-;; with SyncTeX enabled
-;; See http://tex.stackexchange.com/questions/11613/launching-an-external-pdf-viewer-from-emacs-auctex-on-a-mac-osx-fails
-(add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
-
-(setq TeX-source-correlate-method 'synctex)
-
-(add-hook 'LaTeX-mode-hook
-      (lambda()
-        (add-to-list 'TeX-expand-list
-             '("%q" skim-make-url))))
-
-(defun skim-make-url () (concat
-        (TeX-current-line)
-        " "
-        (expand-file-name (funcall file (TeX-output-extension) t)
-            (file-name-directory (TeX-master-file)))
-        " "
-        (buffer-file-name)))
-
-(setq TeX-view-program-list
-      '(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline %q")))
-
-(setq TeX-view-program-selection '((output-pdf "Skim")))
+;; add `arara` to the TeX command list
+(eval-after-load "tex"
+  '(add-to-list 'TeX-command-list
+		'("Arara" "arara --verbose %s" TeX-run-command t t :help "Run arara") t))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
