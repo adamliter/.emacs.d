93123e8ab1661c157943f517a89807e97c8f82b0
Adam Liter <io@adamliter.org>
2018-08-04 10:59:06 -0400

    Don't delete trailing whitespace
    Enabling this globally proved to be a problem when editing hunks with
    `git add -p`, for example. I should at some point investigate more
    conservative options, such as ws-trim or ws-butler.

--- init.el
+++ init.el
@@ -175,5 +175,17 @@
 (show-paren-mode 1)
 (setq-default indent-tabs-mode nil)
 (setq require-final-newline t)
-(setq delete-trailing-lines nil)
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ace-window yasnippet use-package org markdown-mode fill-column-indicator exec-path-from-shell avy))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
