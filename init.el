41d79a99e67e943c0a10c7a4efdf7af9706797d4
Adam Liter <io@adamliter.org>
2018-08-04 17:50:43 -0400

    Add magit

--- init.el
+++ init.el
@@ -98,6 +98,16 @@
     (setq org-default-notes-file (concat org-directory "/refile.org"))
     :pin org)
 (advice-remove 'package-installed-p #'package-from-archive)
+(use-package magit
+  :ensure t
+  :bind (("C-x g" . magit-status))
+  :config
+  (setq git-commit-summary-max-length 50)
+  (setq git-commit-fill-column 72)
+  (setq magit-log-arguments (quote ("--graph"
+                                    "--decorate"
+                                    "--color")))
+  (setq magit-commit-arguments (quote ("--gpg-sign=98723A2089026CD6"))))
 (use-package which-key
   :ensure t
   :config
@@ -179,17 +189,3 @@
 (show-paren-mode 1)
 (setq-default indent-tabs-mode nil)
 (setq require-final-newline t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (which-key ace-window yasnippet use-package org markdown-mode fill-column-indicator exec-path-from-shell avy))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
