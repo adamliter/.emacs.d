2251e3928b40b962b0572851300ff71e8a9d8bbb
Adam Liter <io@adamliter.org>
2018-08-10 18:24:48 -0400

    Use Emacs 26 line number mode
    If it's not available, then just don't display the line numbers, since
    that information is still in the modeline.

--- init.el
+++ init.el
@@ -201,25 +201,9 @@
 (setq initial-scratch-message nil)
 (set-fringe-mode '(0 . 0))
 (load-theme 'manoj-dark t)
-(set-frame-parameter (selected-frame) 'alpha '(90 90))
-(add-to-list 'default-frame-alist '(alpha 90 90))
-(global-linum-mode 1)
-(setq linum-format "%4d \u2502")
+(when (>= emacs-major-version 26)
+  (global-display-line-numbers-mode))
 (setq column-number-mode t)
 (show-paren-mode 1)
 (setq-default indent-tabs-mode nil)
 (setq require-final-newline t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (spaceline-all-the-icons spaceline all-the-icons yasnippet which-key use-package org multi-term markdown-mode magit fill-column-indicator exec-path-from-shell eterm-256color ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
