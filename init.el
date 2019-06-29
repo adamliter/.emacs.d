0dfb51effd21cca7779912d81b4ffcec4ec33a03
Adam Liter <io@adamliter.org>
2019-06-29 18:06:22 -0400

    Don't use icons in dired mode

--- init.el
+++ init.el
@@ -240,11 +240,6 @@ _vr_ reset      ^^                       ^^                 ^^
   :after treemacs projectile
   :ensure t)
 
-(use-package treemacs-icons-dired
-  :after treemacs dired
-  :ensure t
-  :config (treemacs-icons-dired-mode))
-
 (use-package treemacs-magit
   :after treemacs magit
   :ensure t)
